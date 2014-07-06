fdf825382d350d21c5392165564a2b72af4511e0
Yassine AZZOUT <92bondstreet@gmail.com>
2014-07-06 21:57:46 +0200

    Start with eslint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,30 @@ module.exports = function(grunt) {
   var chalk = require('chalk');
   var error = chalk.bold.red;
 
+  // Properties
+  var MODE_BOTH = 'both';
+  var MODE_JSHINT = 'jshint';
+  var MODE_ESLINT = 'eslint';
+
+
   // Get arguments: js files path and .jshintrc path
   var jsPath = grunt.option('js') || null;
-  var config = grunt.option('config') || null;
+  var jsconfig = grunt.option('jsconfig') || null;
+  var esconfig = grunt.option('esconfig') || null;
+  var mode = grunt.option('mode') || MODE_JSHINT;
 
-  // Wording
+  // Define tasks for watch
+  var watchTasks = [MODE_JSHINT];
+  if(mode === MODE_BOTH){
+    watchTasks = [MODE_JSHINT,MODE_ESLINT];
+  }
+  else if(mode === MODE_ESLINT){
+    watchTasks = [MODE_ESLINT];
+  }
+
+  console.log(watchTasks);
+
+  // Wording and missing parameters errors
   var missingJsPath = error('Please use grunt --js {path} where {path} is the path of .*js files to watch and jshint');
 
   if (jsPath === null) {
@@ -16,29 +35,34 @@ module.exports = function(grunt) {
     return false;
   }
 
-
+  // Task definition
   grunt.initConfig({
 
     jshint: {
       options: {
-        jshintrc: config
+        jshintrc: jsconfig
         ,reporter: require('jshint-stylish')
       }
       ,all: {
         src: jsPath
       }
     }
-    ,watch: {
-      jshint: {
-        files: jsPath
-        ,tasks: 'jshint'
-      }
+    ,eslint: {                               
+        target: ['/Users/92bondstreet/Web Development/2014/demographics/js/AgeRange.js']              
     }
+  });
 
+  // Watch task
+  grunt.config('watch', {
+      scripts: {
+        files: jsPath
+        ,tasks: ['eslint','jshint']
+      }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-eslint');
 
   grunt.registerTask('default', ['watch']);
 
